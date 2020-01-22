e46a354ba123f8b9ab7439857fb23a9b2d323e1e
volvos <volvosum@gmail.com>
2020-01-22 19:06:10 +0300

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+    let name=null
+    let author=null
+
+	let songs=[
+		{
+			name:'Good Vibe',
+			author:'Jfla'
+		},
+		{
+			name:'Different World',
+			author:'Alan Walker'
+		},
+		{
+			name:'City of Stars',
+			author:'La La Land'
+		},
+		{
+			name:'High Hopes',
+			author:'Panic! At The Disco'
+		}
+	]
+
+</script>
---
+++ src/App2.svelte
@@ -0,0 +1,28 @@
+<script>
+    let name
+    let singer
+    let frameworks = []
+    let gender
+</script>
+
+<form>
+    <input type="text" bind:value={name}>
+    <hr>
+    <select bind:value={singer}>
+        <option>Ali</option>
+        <option>Veli</option>
+        <option>Deli</option>
+    </select>
+    <hr>
+    <input type="checkbox" bind:group={frameworks} value="Svelte"> Svelte
+    <input type="checkbox" bind:group={frameworks} value="Vue"> Vue
+    <input type="checkbox" bind:group={frameworks} value="React"> React
+    <hr>
+    <input type="radio" bind:group={gender} value="Woman">Woman
+    <input type="radio" bind:group={gender} value="Man">Man
+</form>
+
+{name} <br />
+{singer} <br />
+{frameworks} <br />
+{gender} <br />
