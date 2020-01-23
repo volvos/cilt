2ee971dda3def5daa00b46ea75be2a263e41c9fa
volvos <volvosum@gmail.com>
2020-01-23 18:05:48 +0300

    Add Song to List

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
-    let name=null
-    let author=null
+    let name=''
+    let author=''
 
 	let songs=[
 		{
@@ -21,4 +21,27 @@
 		}
 	]
 
+	function clearInputs(){
+		name=''
+		author=''
+	}
+
+	function addSongList(){
+		songs=[...songs, {name:name, author:author}]
+		clearInputs()
+	}
 </script>
+<div class="container">
+	<ul class="list-group">
+		{#each songs as song}
+			<li class="list-group-item">{song.name}
+			<b class="float-right">{song.author}</b></li>
+		{/each}
+	</ul>
+
+	<form class="mt-2" on:submit|preventDefault={addSongList}>
+		<input type="text" class="form-control" bind:value={name} placeholder="Song Name">
+		<input type="text" class="form-control" bind:value={author} placeholder="Author">
+		<button class="btn btn-outline-primary btn-block" disabled={name==''|author==''}>Add</button>
+	</form>
+</div>
